c36b4f28327bd0eda6c52fa80d16ebf77a25d6c8
dragazo <37459229+dragazo@users.noreply.github.com>
2018-10-16 13:06:40 -0500

    rect select + optimization
    added rectangle selection for left click + drag.
    currently, the selection does nothing (not used by anything and no visual effect to know if something is rendered) - this will be added in the next update for graphics.
    
    optimized the code used by this and drag action to not perform redundant frame updates.
    
    added helper functions for QRectF creation/manipulation.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include <QCursor>
 #include <QPainter>
 #include <cmath>
+#include <QStatusBar>
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
@@ -38,15 +39,80 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+// ------------- //
+
+// -- utility -- //
+
+// ------------- //
+
+QRectF MainWindow::boundingRect(QPointF a, QPointF b)
+{
+    return QRectF(std::min(a.x(), b.x()), std::min(a.y(), b.y()),
+                  std::abs(a.x() - b.x()), std::abs(a.y() - b.y()));
+}
+QRectF MainWindow::inflate(QRectF rect, qreal padding)
+{
+    return QRectF(rect.x() - padding, rect.y() - padding,
+                  rect.width() + 2 * padding, rect.height() + 2 * padding);
+}
+
+bool MainWindow::intersects(QRectF rect, QPointF point)
+{
+    return point.x() >= rect.left() && point.x() <= rect.right()
+            && point.y() >= rect.top() && point.y() <= rect.bottom();
+}
+
+MainWindow::Map_t::iterator MainWindow::overNode(QPointF point)
+{
+    // we'll do the distance comparison in terms of squares for speed
+    const auto sqr_radius = NodeRadius * NodeRadius;
+
+    // for each node
+    QPointF d;
+    auto i = map.begin(), _end = map.end();
+    for (; i != _end; ++i)
+    {
+        // compute position difference
+        d = point - i->data.point;
+
+        // if we're within this node, stop searching
+        if (d.x() * d.x() + d.y() * d.y() <= sqr_radius) break;
+    }
+
+    // return the found position
+    return i;
+}
+
+void MainWindow::performSelect(QRectF rect, bool add)
+{
+    // if we're not adding, start by clearing the selection
+    if (!add) selection.clear();
+
+    // for each node
+    for (auto i = map.begin(), _end = map.end(); i != _end; ++i)
+    {
+        // if this is in the rectangle
+        if (intersects(rect, i->data.point))
+        {
+            // add it to the selection
+            selection.push_back(i);
+        }
+    }
+}
+
+// --------------- //
+
 // -- rendering -- //
 
-void MainWindow::_paintNode(const Node_t &node, QPainter &painter)
+// --------------- //
+
+void MainWindow::paintNode(const Node_t &node, QPainter &painter)
 {
     painter.drawEllipse(QRectF(node.data.point.x() - NodeRadius, node.data.point.y() - NodeRadius, 2*NodeRadius, 2*NodeRadius));
 
     painter.drawText(node.data.point, node.data.text);
 }
-void MainWindow::_paintArc(const Node_t &from_node, const Arc_t &arc, QPainter &painter)
+void MainWindow::paintArc(const Node_t &from_node, const Arc_t &arc, QPainter &painter)
 {
     // if this arc is valid
     if (arc.dest < map.size())
@@ -95,38 +161,28 @@ void MainWindow::paintEvent(QPaintEvent *e)
     QPainter painter(this);
 
     // paint each node
-    for (const auto &i : map) _paintNode(i, painter);
+    for (const auto &i : map) paintNode(i, painter);
 
     QBrush brush(QColor(197, 70, 250));
     QPen arc_pen(brush, 4, Qt::PenStyle::SolidLine, Qt::PenCapStyle::FlatCap, Qt::PenJoinStyle::MiterJoin);
     painter.setPen(arc_pen);
 
     // paint each arc
-    for (const auto &i : map) for (const auto &j : i.arcs) _paintArc(i, j, painter);
+    for (const auto &i : map) for (const auto &j : i.arcs) paintArc(i, j, painter);
+
+    // if we're in a selection
+    if (select_timer_id != 0)
+    {
+        // paint the selection rect
+        painter.drawRect(boundingRect(select_mouse_start, select_mouse_stop));
+    }
 }
+
+// -------------- //
 
 // -- controls -- //
 
-MainWindow::Map_t::iterator MainWindow::_overNode(QPointF point)
-{
-    // we'll do the distance comparison in terms of squares for speed
-    const auto sqr_radius = NodeRadius * NodeRadius;
-
-    // for each node
-    QPointF d;
-    auto i = map.begin(), _end = map.end();
-    for (; i != _end; ++i)
-    {
-        // compute position difference
-        d = point - i->data.point;
-
-        // if we're within this node, stop searching
-        if (d.x() * d.x() + d.y() * d.y() <= sqr_radius) break;
-    }
-
-    // return the found position
-    return i;
-}
+// -------------- //
 
 void MainWindow::_begin_drag(Map_t::iterator node, QPointF mouse_start)
 {
@@ -137,6 +193,7 @@ void MainWindow::_begin_drag(Map_t::iterator node, QPointF mouse_start)
         drag_node = node;
         drag_node_start = node->data.point;
         drag_mouse_start = mouse_start;
+        drag_mouse_stop = mouse_start;
 
         // start the timer
         drag_timer_id = startTimer(DragSleepTime);
@@ -144,9 +201,15 @@ void MainWindow::_begin_drag(Map_t::iterator node, QPointF mouse_start)
 }
 void MainWindow::_mid_drag(QPointF mouse_stop)
 {
-    // perform the node repositioning
-    drag_node->data.point = drag_node_start + (mouse_stop - drag_mouse_start);
-    update();
+    // for efficiency, only do this if the mouse moved
+    if (drag_mouse_stop != mouse_stop)
+    {
+        drag_mouse_stop = mouse_stop;
+
+        // perform the node repositioning
+        drag_node->data.point = drag_node_start + (mouse_stop - drag_mouse_start);
+        update();
+    }
 }
 void MainWindow::_end_drag(QPointF mouse_stop)
 {
@@ -167,95 +230,137 @@ void MainWindow::_cancel_drag()
     _end_drag(drag_mouse_start);
 }
 
-void MainWindow::_prompt_editor(Map_t::iterator node)
+void MainWindow::_begin_select(QPointF mouse_start)
+{
+    // for safety only do this if we're not in a select event
+    if (select_timer_id == 0)
+    {
+        // record the initial data
+        select_mouse_start = mouse_start;
+        select_mouse_stop = mouse_start;
+
+        // start the timer
+        select_timer_id = startTimer(SelectSleepTime);
+    }
+}
+void MainWindow::_mid_select(QPointF mouse_stop)
+{
+    // for efficiency, only do this if the mouse moved
+    if (select_mouse_stop != mouse_stop)
+    {
+        select_mouse_stop = mouse_stop;
+
+        // redraw
+        update();
+    }
+}
+void MainWindow::_end_select(QPointF mouse_stop)
+{
+    // for safety, only do this if we're in a select action
+    if (select_timer_id != 0)
+    {
+        // stop the timer
+        killTimer(select_timer_id);
+        select_timer_id = 0;
+
+        // perform the selection
+        performSelect(boundingRect(select_mouse_start, mouse_stop), false);
+
+        // update the frame
+        update();
+    }
+}
+void MainWindow::_cancel_select()
+{
+    // for safety, only do this if we're in a select action
+    if (select_timer_id != 0)
+    {
+        // stop the timer
+        killTimer(select_timer_id);
+        select_timer_id = 0;
+
+        // redraw
+        update();
+    }
+}
+
+void MainWindow::prompt_editor(Map_t::iterator node)
 {
     // create the editor
     NodeEditor editor;
 
+    // provide editor with the current data
     editor.title(node->data.title);
     editor.text(node->data.text);
 
     // if the user says ok, store the changes
     if (editor.exec() == QDialog::Accepted)
     {
+        // save the new data
         node->data.title = editor.title();
         node->data.text = editor.text();
 
+        // redraw with new data
         update();
     }
 }
 
 void MainWindow::mousePressEvent(QMouseEvent *e)
 {
-    // default to ignoring the event
-    e->ignore();
-
     // if this was a left click
     if (e->button() == Qt::MouseButton::LeftButton)
     {
-        // get the node we're over
-        auto node = _overNode(e->pos());
-        // if we were indeed over a node
-        if (node != map.end())
-        {
-            // begin a drag
-            _begin_drag(node, e->pos());
+        // get the node we're over (may not be)
+        auto node = overNode(e->pos());
 
-            e->accept();
-        }
+        // if we were over a node, begin a drag
+        if (node != map.end()) _begin_drag(node, e->pos());
+        // otherwise begin a selection
+        else _begin_select(e->pos());
     }
+
+    e->accept();
 }
 void MainWindow::mouseReleaseEvent(QMouseEvent *e)
 {
-    // default to ignoring the event
-    e->ignore();
-
     // if the released button was a left click
     if (e->button() == Qt::MouseButton::LeftButton)
     {
-        // if we were in a drag action
-        if (drag_timer_id != 0)
-        {
-            // end the drag
-            _end_drag(e->pos());
-
-            e->accept();
-        }
+        // if we were in a drag action, end it
+        if (drag_timer_id != 0) _end_drag(e->pos());
+        // if we were in a select action, end it
+        if (select_timer_id != 0) _end_select(e->pos());
     }
+
+    e->accept();
 }
 
 void MainWindow::mouseDoubleClickEvent(QMouseEvent *e)
 {
-    // default to ignoring the event
-    e->ignore();
-
     // if this was a left click
     if (e->button() == Qt::MouseButton::LeftButton)
     {
         // cancel any drag action we might be in
         _cancel_drag();
 
-        // get the node we're over
-        auto node = _overNode(e->pos());
-        // if we were indeed over a node
-        if (node != map.end())
-        {
-            // open the editor for this node
-            _prompt_editor(node);
+        // get the node we're over (may not be)
+        auto node = overNode(e->pos());
 
-            e->accept();
-        }
+        // if we were over a node, open an editor for it
+        if (node != map.end()) prompt_editor(node);
     }
+
+    e->accept();
 }
 
 void MainWindow::timerEvent(QTimerEvent *e)
 {
-    // if the timer was for a drag action
-    if (e->timerId() == drag_timer_id)
-    {
-        // update the animation
-        _mid_drag(mapFromGlobal(QCursor::pos()));
-    }
+    // handle drag action
+    if (e->timerId() == drag_timer_id) _mid_drag(mapFromGlobal(QCursor::pos()));
+    // handle select action
+    else if(e->timerId() == select_timer_id) _mid_select(mapFromGlobal(QCursor::pos()));
+
+    e->accept();
 }
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -25,7 +25,8 @@ private: // -- settings -- //
     static constexpr qreal ArrowHeight = 16; // the height of an arc arrow
     static constexpr qreal ArrowRecess = 5;  // the recess amount for the very tip of the arrow
 
-    static constexpr int DragSleepTime = 16; // the frequency of drag action frame updates (milliseconds)
+    static constexpr int DragSleepTime = 16;   // the frequency of drag   action frame updates (milliseconds)
+    static constexpr int SelectSleepTime = 16; // the frequency of select action frame updates (milliseconds)
 
 private: // -- types -- //
 
@@ -51,10 +52,17 @@ private: // -- data -- //
 
     Map_t map; // the adventure map to use for execution/rendering
 
-    int drag_timer_id = 0;     // the timer if for the drag updater (zero if we're not in a drag event)
+    int drag_timer_id = 0;     // the timer for the drag updater (zero if we're not in a drag event)
     Map_t::iterator drag_node; // iterator to the node being dragged (undefined if not in drag action)
     QPointF drag_node_start;   // the position of the node when the drag started (und if not in drag)
     QPointF drag_mouse_start;  // the position of the mouse when the drag started (und. etc.)
+    QPointF drag_mouse_stop;   // the current stop position of the drag action.
+
+    int select_timer_id = 0;    // the timer for the select updater (zero if we're not in a select event)
+    QPointF select_mouse_start; // the mouse start point for a select event
+    QPointF select_mouse_stop;  // the current stop position of the select action.
+
+    std::vector<Map_t::iterator> selection; // all the nodes that are currently selected
 
 public: // -- ctor / dtor / asgn -- //
 
@@ -63,21 +71,39 @@ public: // -- ctor / dtor / asgn -- //
 
 private: // -- helpers -- //
 
-    // helpers for painting nodes and arcs
-    void _paintNode(const Node_t &node, QPainter &paint);
-    void _paintArc(const Node_t &from_node, const Arc_t &arc, QPainter &paint);
+    // returns the smallest rectangle containing the specified points
+    static QRectF boundingRect(QPointF a, QPointF b);
+    // inflates the rectangle by <padding> on all sides
+    static QRectF inflate(QRectF rect, qreal padding);
+
+    // returns true iff the rect intersects the given point
+    static bool intersects(QRectF rect, QPointF point);
 
     // finds the (first) node that the given point is within. returns map.end() if there is no such node.
-    Map_t::iterator _overNode(QPointF point);
+    Map_t::iterator overNode(QPointF point);
 
-    // these process node drag begin/end
+    // performs a selection action for every node in the given rectangle.
+    // if add is true, adds items to the current selection, otherwise clears current selection before adding the new selection.
+    void performSelect(QRectF rect, bool add);
+
+    // helpers for painting nodes and arcs
+    void paintNode(const Node_t &node, QPainter &paint);
+    void paintArc(const Node_t &from_node, const Arc_t &arc, QPainter &paint);
+
+    // these process node drag subactions
     void _begin_drag(Map_t::iterator node, QPointF mouse_start);
     void _mid_drag(QPointF mouse_stop);
     void _end_drag(QPointF mouse_stop);
     void _cancel_drag();
 
+    // these process select subactions
+    void _begin_select(QPointF mouse_start);
+    void _mid_select(QPointF mouse_stop);
+    void _end_select(QPointF mouse_stop);
+    void _cancel_select();
+
     // opens an editor interface for the given node
-    void _prompt_editor(Map_t::iterator node);
+    void prompt_editor(Map_t::iterator node);
 
 protected: // -- event overrides -- //
 
